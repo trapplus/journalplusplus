18a9a9809113c601227149a45be1a70d566460fa
trapplus <blazer19092008@gmail.com>
2026-03-22 22:01:38 +0300

    remove old bloatcode

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import asyncio
-import logging
-import sys
+def main():
+    print("Hello from journalplusplus!")
 
-from src import container as cont
-
-
-async def main() -> None:
-    
-    await cont.objects.dp.start_polling(cont.objects.bot)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-    asyncio.run(main())
+    main()
